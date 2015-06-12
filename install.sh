a59e569acd51c3e8f13e2e195c9e10b60ef7ca73
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-06-12 10:55:53 -0700

    Added draft of install script.

---
+++ install.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/tylersuzukinelson/OFiDT.git
+cd OFiDT/
+git clone https://github.com/idonethis/didit-cli-client.git
+cd didit-cli-client/
+bundle install
+cd ..
+echo "Time to configure your iDoneThis. You will need your API token from https://idonethis.com/api/token/"
+echo "When you have entered your API token, click the enter/return key twice."
+./didit-cli-client/didit.rb
+# TK Set up cron job
+echo "Assuming no errors, installation complete."
